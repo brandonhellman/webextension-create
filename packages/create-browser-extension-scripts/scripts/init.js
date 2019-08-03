bdb8a7e76c83f608c36fa75ccd722d5003732681
Kadauchi <kadauchi@gmail.com>
2019-08-03 10:33:54 -0700

    Add scripts to the package and copy the template.

--- packages/create-browser-extension-scripts/scripts/init.js
+++ packages/create-browser-extension-scripts/scripts/init.js
@@ -1,1 +1,16 @@
-console.log('create-browser-extension-scripts init ran');
+const fs = require('fs-extra');
+const path = require('path');
+
+const getTemplatePath = require('./utils/getTemplatePath');
+
+const projectRoot = process.cwd();
+const packageJson = fs.readJsonSync(path.join(projectRoot, 'package.json'));
+const templatePath = getTemplatePath(packageJson);
+
+packageJson.scripts = {
+  build: 'create-browser-extension-scripts build',
+  dist: 'create-browser-extension-scripts dist',
+};
+
+fs.outputJsonSync(path.join(projectRoot, 'package.json'), packageJson, { spaces: 2 });
+fs.copySync(templatePath, projectRoot);
