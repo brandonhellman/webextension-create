6a681a62ee70d6d5258afb5cfbeeeda8a54181f3
Kadauchi <kadauchi@gmail.com>
2019-08-03 11:03:24 -0700

    Fixed path to the template.

--- packages/create-browser-extension-scripts/scripts/utils/getTemplatePath.js
+++ packages/create-browser-extension-scripts/scripts/utils/getTemplatePath.js
@@ -2,18 +2,19 @@ const path = require('path');
 
 module.exports = function getTemplatePath(packageJson) {
   const dependencies = packageJson.dependencies || {};
+  const templatesPath = [__dirname, '..', '..', 'templates'];
 
   if (dependencies.react && dependencies.typescript) {
-    return path.join(__dirname, '..', 'templates', 'react-typescript');
+    return path.join(...templatesPath, 'react-typescript');
   }
 
   if (dependencies.react) {
-    return path.join(__dirname, '..', 'templates', 'react');
+    return path.join(...templatesPath, 'react');
   }
 
   if (dependencies.typescript) {
-    return path.join(__dirname, '..', 'templates', 'typescript');
+    return path.join(...templatesPath, 'typescript');
   }
 
-  return path.join(__dirname, '..', 'templates', 'js');
+  return path.join(...templatesPath, 'js');
 };
