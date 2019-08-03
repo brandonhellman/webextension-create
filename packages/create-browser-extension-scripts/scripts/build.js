f57168b1074275de20e30af59d79d860b9720c69
Kadauchi <kadauchi@gmail.com>
2019-08-03 14:52:51 -0700

    Run webpack on build.

--- packages/create-browser-extension-scripts/scripts/build.js
+++ packages/create-browser-extension-scripts/scripts/build.js
@@ -1,1 +1,9 @@
-console.log('create-browser-extension-scripts build ran');
+const webpack = require('webpack');
+
+const webpackConfig = require('../webpack/webpack.config');
+
+const compiler = webpack(webpackConfig);
+
+compiler.run((err, stats) => {
+  console.log(err ? 'error' : stats);
+});
