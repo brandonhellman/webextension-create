0d5c2393b75f07d7289725969ddbdd234fd618d6
Kadauchi <kadauchi@gmail.com>
2019-08-02 22:03:55 -0700

    Determine which script to run.

--- packages/create-browser-extension-scripts/index.js
+++ packages/create-browser-extension-scripts/index.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+const args = process.argv.slice(2);
+const script = args[0];
+
+switch (script) {
+  case 'build':
+  case 'dist':
+  case 'init':
+    require(`./scripts/${script}`);
+    break;
+  default:
+    console.log(`Unknown script "${script}".`);
+    break;
+}
