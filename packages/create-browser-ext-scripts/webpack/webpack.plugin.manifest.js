a8fdddb1a1a021a40914de036153c9948647256f
Kadauchi <kadauchi@gmail.com>
2019-08-03 19:42:20 -0700

    Removed the transform.

--- packages/create-browser-ext-scripts/webpack/webpack.plugin.manifest.js
+++ packages/create-browser-ext-scripts/webpack/webpack.plugin.manifest.js
@@ -3,14 +3,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const extPath = require('../utils/getExtPath');
 
-// Copies the manifest.json file and merges properties from the package.json file.
 module.exports = new CopyWebpackPlugin([
   {
     from: path.join(extPath, 'src', 'manifest.json'),
     to: path.join(extPath, 'build', 'manifest.json'),
-    transform(content) {
-      const manifest = content.toString();
-      return JSON.stringify(manifest, null, 2);
-    },
   },
 ]);
