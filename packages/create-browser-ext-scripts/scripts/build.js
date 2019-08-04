b2702a4b2ea0918929d474c196bb629681e30155
Kadauchi <kadauchi@gmail.com>
2019-08-04 14:23:29 -0700

    Handle errors and warnings from webpack.

--- packages/create-browser-ext-scripts/scripts/build.js
+++ packages/create-browser-ext-scripts/scripts/build.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const webpackFormatMessages = require('webpack-format-messages');
 
 const webpackConfig = require('../webpack/webpack.config');
 
@@ -10,6 +11,16 @@ compiler.run((err, stats) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Building finished in ${stats.endTime - stats.startTime}ms.`);
+    const messages = webpackFormatMessages(stats);
+
+    if (!messages.errors.length && !messages.warnings.length) {
+      console.log(`Compiled successfully in ${stats.endTime - stats.startTime}ms!'`);
+    } else if (messages.errors.length) {
+      console.log('Failed to compile.');
+      messages.errors.forEach((e) => console.log(e));
+    } else if (messages.warnings.length) {
+      console.log(`Compiled with warnings in ${stats.endTime - stats.startTime}ms,`);
+      messages.warnings.forEach((w) => console.log(w));
+    }
   }
 });
