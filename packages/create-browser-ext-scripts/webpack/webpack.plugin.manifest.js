5930eb7f1b727b051f85d6babb7b86283c469b93
Kadauchi <kadauchi@gmail.com>
2019-08-03 20:50:22 -0700

    Replace __package.version__ with the package.json version.

--- packages/create-browser-ext-scripts/webpack/webpack.plugin.manifest.js
+++ packages/create-browser-ext-scripts/webpack/webpack.plugin.manifest.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const extPackage = require('../utils/getExtPackage');
 const extPath = require('../utils/getExtPath');
 
 module.exports = new CopyWebpackPlugin([
   {
     from: path.join(extPath, 'src', 'manifest.json'),
     to: path.join(extPath, 'build', 'manifest.json'),
+    transform(content) {
+      return content.toString().replace('__package.version__', extPackage.version);
+    },
   },
 ]);
