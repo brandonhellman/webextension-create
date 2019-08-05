9ea9fbc78fe51d04548640cb20781a9e868a39ea
Kadauchi <kadauchi@gmail.com>
2019-08-05 11:40:09 -0700

    Fixed pathing to read, skip, zip files.

--- packages/create-browser-ext-scripts/utils/zipUnpacked.js
+++ packages/create-browser-ext-scripts/utils/zipUnpacked.js
@@ -4,6 +4,7 @@ const JSZip = require('jszip');
 const recursiveReaddirSync = require('recursive-readdir-sync');
 
 const extPath = require('./getExtPath');
+const buildPath = path.join(extPath, 'build');
 const buildFilePaths = recursiveReaddirSync(path.join(extPath, 'build', 'unpacked'));
 const package = fs.readJsonSync('./package.json');
 
@@ -16,14 +17,14 @@ const package = fs.readJsonSync('./package.json');
 
   if (target === 'chrome') {
     buildFilePaths.forEach((filePath) => {
-      const parsed = path.parse(filePath);
-      const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'build');
+      const parsed = path.parse(filePath.replace(buildPath, ''));
+      const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'unpacked');
       zip.folder(zipName).file(path.join(...dirs, parsed.base), fs.readFileSync(filePath));
     });
   } else if (target === 'firefox') {
     buildFilePaths.forEach((filePath) => {
-      const parsed = path.parse(filePath);
-      const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'build');
+      const parsed = path.parse(filePath.replace(buildPath, ''));
+      const dirs = parsed.dir.split(path.sep).filter((dir) => dir !== 'unpacked');
       zip.file(path.join(...dirs, parsed.base), fs.readFileSync(filePath));
     });
   }
@@ -34,7 +35,7 @@ const package = fs.readJsonSync('./package.json');
     .on('finish', () => console.log(`${zipPath} created.`));
 });
 
-const sourceFilePaths = recursiveReaddirSync(path.join(extPath));
+const sourceFilePaths = recursiveReaddirSync(extPath);
 const sourceZip = new JSZip();
 const sourceZipName = `source-${package.version}`;
 const sourceZipPath = `build/${sourceZipName}.zip`;
@@ -44,11 +45,11 @@ fs.removeSync(sourceZipPath);
 sourceFilePaths.forEach((filePath) => {
   const pathsToSkip = ['.git', 'build', 'node_modules'];
 
-  if (pathsToSkip.some((pathToSkip) => path.join(filePath).startsWith(path.join(extPath, pathToSkip)))) {
+  if (pathsToSkip.some((pathToSkip) => filePath.replace(extPath + path.sep, '').startsWith(pathToSkip))) {
     return;
   }
 
-  sourceZip.file(filePath, fs.readFileSync(filePath));
+  sourceZip.file(filePath.replace(extPath + path.sep, ''), fs.readFileSync(filePath));
 });
 
 sourceZip
