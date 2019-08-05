e2d374fe075b0b2fa37aa7186e72fbb7cc0afa55
Kadauchi <kadauchi@gmail.com>
2019-08-05 08:34:29 -0700

    Manually pull entries from the manifest.

--- packages/create-browser-ext-scripts/webpack/webpack.plugin.reloader.js
+++ packages/create-browser-ext-scripts/webpack/webpack.plugin.reloader.js
@@ -1,10 +1,20 @@
+const fs = require('fs-extra');
 const path = require('path');
 const ExtensionReloader = require('webpack-extension-reloader');
 
 const extPath = require('../utils/getExtPath');
+const extManifest = fs.readJsonSync(path.join(extPath, 'src', 'manifest.json'));
+
+const content_scripts = extManifest.content_scripts
+  ? extManifest.content_scripts.reduce((acc, script) => {
+      const names = script.js.map((file) => 'content_scripts/' + path.parse(file).name);
+      return [...acc, ...names];
+    }, [])
+  : [];
 
 module.exports = new ExtensionReloader({
-  manifest: path.resolve(extPath, 'build', 'manifest.json'),
-  port: 1111,
-  reloadPage: true,
+  entries: {
+    background: 'pages/' + path.parse(extManifest.background.scripts[0]).name,
+    contentScript: content_scripts,
+  },
 });
