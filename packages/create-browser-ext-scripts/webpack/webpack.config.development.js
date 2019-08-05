f8120f8c9a70ec707f568e0cc65eb85e1a4bf74c
Kadauchi <kadauchi@gmail.com>
2019-08-04 19:24:17 -0700

    Create webpack.config.development.js

---
+++ packages/create-browser-ext-scripts/webpack/webpack.config.development.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+const content_scripts = require('./webpack.entry.content_scripts');
+const pages = require('./webpack.entry.pages');
+const html = require('./webpack.plugin.html');
+const manifest = require('./webpack.plugin.manifest');
+const public = require('./webpack.plugin.public');
+const reloader = require('./webpack.plugin.reloader');
+
+const extPath = require('../utils/getExtPath');
+
+module.exports = {
+  entry: {
+    ...content_scripts,
+    ...pages,
+  },
+
+  output: {
+    path: extPath + '/build',
+  },
+
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx|mjs)$/,
+        exclude: /node_modules/,
+        include: path.join(extPath, 'src'),
+        use: {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              require.resolve('@babel/preset-env'),
+              require.resolve('@babel/preset-react'),
+              require.resolve('@babel/preset-typescript'),
+            ],
+          },
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: require.resolve('source-map-loader'),
+      },
+      {
+        test: /\.css$/,
+        use: [require.resolve('style-loader'), require.resolve('css-loader')],
+      },
+    ],
+  },
+
+  plugins: [...html, manifest, public, reloader],
+};
