f4b4b0ec1a7ff9b054421d02a99635b3f07dbd07
Kadauchi <kadauchi@gmail.com>
2019-08-07 14:07:55 -0700

    Zip unpacked folder even when compiled with errors.

--- packages/web-extension-scripts/scripts/build.js
+++ packages/web-extension-scripts/scripts/build.js
@@ -13,15 +13,18 @@ compiler.run((err, stats) => {
   } else {
     const messages = webpackFormatMessages(stats);
 
-    if (!messages.errors.length && !messages.warnings.length) {
-      console.log(`Compiled successfully in ${stats.endTime - stats.startTime}ms!`);
-      require('../utils/zipUnpacked');
-    } else if (messages.errors.length) {
+    if (messages.errors.length) {
       console.log('Failed to compile.');
       messages.errors.forEach((e) => console.log(e));
-    } else if (messages.warnings.length) {
-      console.log(`Compiled with warnings in ${stats.endTime - stats.startTime}ms,`);
-      messages.warnings.forEach((w) => console.log(w));
+    } else {
+      if (messages.warnings.length) {
+        console.log(`Compiled with warnings in ${stats.endTime - stats.startTime}ms,`);
+        messages.warnings.forEach((w) => console.log(w));
+      } else {
+        console.log(`Compiled successfully in ${stats.endTime - stats.startTime}ms!`);
+      }
+
+      require('../utils/zipUnpacked');
     }
   }
 });
