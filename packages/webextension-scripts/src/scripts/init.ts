085563cb0980d0a8d88e5553d979104592c38292
Kadauchi <kadauchi@gmail.com>
2019-08-13 10:43:28 -0700

    Fixed strict mode error.
    package is a reserved name

--- packages/webextension-scripts/src/scripts/init.ts
+++ packages/webextension-scripts/src/scripts/init.ts
@@ -1,21 +1,21 @@
 const fs = require('fs-extra');
 
-const package = require('../utils/packageJson');
+const packageJson = require('../utils/packageJson');
 const paths = require('../utils/paths');
 
 require('../utils/tsconfigSetup');
 
-package.scripts = {
+packageJson.scripts = {
   start: 'webextension-scripts start',
   build: 'webextension-scripts build',
 };
 
-fs.outputJsonSync(paths.extPackageJson, package, { spaces: 2 });
+fs.outputJsonSync(paths.extPackageJson, packageJson, { spaces: 2 });
 
 fs.copySync(paths.templatesGitignore, paths.extGitignore);
 fs.copySync(paths.templatesReadme, paths.extReadme);
 
-const dependencies = package.dependencies || {};
+const dependencies = packageJson.dependencies || {};
 
 if (dependencies.react && dependencies.typescript) {
   fs.copySync(paths.templatesReactTypescript, paths.extRoot);
