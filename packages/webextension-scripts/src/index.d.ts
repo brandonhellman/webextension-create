e7ba1b784fd66c2a8c713f52fd5b9a2c0ae24fe7
Kadauchi <kadauchi@gmail.com>
2019-08-13 10:43:54 -0700

    Split out declarations.

---
+++ packages/webextension-scripts/src/index.d.ts
@@ -0,0 +1,29 @@
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
