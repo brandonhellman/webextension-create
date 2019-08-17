6904110c4d1872f9853c9bfadf7cfc162b9fd4de
Kadauchi <kadauchi@gmail.com>
2019-08-17 16:44:00 -0700

    Handle mutliple commands withcommander.

--- packages/webextension-scripts/src/index.ts
+++ packages/webextension-scripts/src/index.ts
@@ -1,16 +1,52 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2);
-const script = args[0];
+import program from 'commander';
+
+const packageJson = require('../package.json');
+
+let script: string | undefined;
+let option: string | undefined;
+
+program
+  .name(packageJson.name)
+  .version(packageJson.version)
+  .usage('<script> [option]');
+
+program
+  .command('build')
+  .description('build folders ready for distribution')
+  .action(() => {
+    script = 'build';
+  });
+
+program
+  .command('init [template]')
+  .description('initialize webextension-scripts in the current directory')
+  .action((template) => {
+    script = 'init';
+    option = template;
+  });
+
+program
+  .command('start [browser]')
+  .description('start developing the web extension')
+  .action((browser) => {
+    script = 'start';
+    option = browser;
+  });
+
+program.parse(process.argv);
 
 switch (script) {
-  case 'start':
   case 'build':
+    require('./scripts/build').default();
+    break;
   case 'init':
-    require('./utils/tsconfigSetup');
-    require(`./scripts/${script}`);
+    require('./scripts/init').default(option);
+    break;
+  case 'start':
+    require('./scripts/start').default(option);
     break;
   default:
-    console.log(`Unknown script "${script}".`);
-    break;
+    program.outputHelp();
 }
