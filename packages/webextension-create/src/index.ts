8fefe3557c7d565def8fadc002108e55ac7270ea
Kadauchi <kadauchi@gmail.com>
2019-08-19 13:51:47 -0700

    Update index.ts

--- packages/webextension-create/src/index.ts
+++ packages/webextension-create/src/index.ts
@@ -13,7 +13,7 @@ const packageJson = fs.readJSONSync(path.join(__dirname, '..', 'package.json'));
 program
   .name(packageJson.name)
   .version(packageJson.version, '-v, --version')
-  .option('-d, --dev', 'Use link instead of install for webextension-scripts')
+  .option('-d, --dev', 'use link instead of install for webextension-scripts')
   .parse(process.argv);
 
 const questions = [
