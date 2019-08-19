9170a7a5f3447ba99f6faccabd6a4d409f1a1a8c
Kadauchi <kadauchi@gmail.com>
2019-08-19 14:59:58 -0700

    Better logic used to see if tsconfig.json needs to be created.

--- packages/webextension-scripts/src/utils/tsconfigSetup.ts
+++ packages/webextension-scripts/src/utils/tsconfigSetup.ts
@@ -5,22 +5,30 @@ import * as ext from '../utils/ext';
 export function tsconfigSetup() {
   const tsconfigExists = fs.existsSync(ext.pathToTsconfigJson);
 
-  if (!tsconfigExists && ext.packageJson.dependencies && ext.packageJson.dependencies.typescript) {
-    const tsconfig = {
-      compilerOptions: {
-        esModuleInterop: true,
-        inlineSourceMap: true,
-        noImplicitAny: true,
-        module: 'commonjs',
-        target: 'es6',
-        jsx: 'react',
-        lib: ['es2018', 'dom'],
-      },
-    };
-
-    fs.outputJsonSync(ext.pathToTsconfigJson, tsconfig, { spaces: 2 });
-
-    console.log();
-    console.log('tsconfig.json setup is complete');
+  if (tsconfigExists) {
+    return;
   }
+
+  const packageJson = ext.getPackageJson();
+
+  if (!packageJson.dependencies || !packageJson.dependencies.typescript) {
+    return;
+  }
+
+  const tsconfig = {
+    compilerOptions: {
+      esModuleInterop: true,
+      inlineSourceMap: true,
+      noImplicitAny: true,
+      module: 'commonjs',
+      target: 'es6',
+      jsx: 'react',
+      lib: ['es2018', 'dom'],
+    },
+  };
+
+  fs.outputJsonSync(ext.pathToTsconfigJson, tsconfig, { spaces: 2 });
+
+  console.log();
+  console.log('tsconfig.json setup is complete');
 }
