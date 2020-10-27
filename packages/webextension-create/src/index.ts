7a33e1149dd094145cc46b8df0e94b096217d01f
Brandon Hellman <me@brandonhellman.com>
2020-10-27 12:45:09 -0700

    Updated packages and attempted fix for arrow keys not working

--- packages/webextension-create/src/index.ts
+++ packages/webextension-create/src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
-import program from 'commander';
 import fs from 'fs-extra';
 import inquirer from 'inquirer';
 import path from 'path';
+import program from 'commander';
 import spawn from 'cross-spawn';
 import validate from 'validate-npm-package-name';
 
@@ -20,9 +20,9 @@ const questions = [
   {
     type: 'input',
     name: 'name',
-    message: 'Enter the name of your project',
+    message: 'Enter the name of your project:',
     default: 'my-webextension',
-    validate(value: string | undefined) {
+    validate(value: any) {
       const valid = validate(value);
 
       if (!valid.validForNewPackages) {
@@ -33,25 +33,26 @@ const questions = [
       const pathExists = value && fs.existsSync(path.resolve(value));
 
       if (pathExists) {
-        return 'The path already exists! Please try a different name or delete the folder.';
+        return `The path ${chalk.green(value)} already exists! Please try a different name or delete the folder.`;
       }
 
       return true;
     },
   },
   {
-    type: 'list',
+    type: /^win/.test(process.platform) ? 'rawlist' : 'list',
     name: 'template',
-    message: 'Choose a webextension template to use',
+    message: 'Choose a webextension template to use:',
     choices: ['javascript', 'react', 'react-typescript', 'typescript'],
     default: 'javascript',
   },
 ];
 
-// @ts-ignore
-inquirer.prompt(questions).then((answers: { name: string; template: string }) => {
-  createBrowserExtension(answers.name, answers.template, program.dev);
-});
+inquirer
+  .prompt(questions)
+  .then((answers: { name: string; template: string }) => {
+    createBrowserExtension(answers.name, answers.template, program.dev);
+  });
 
 function createBrowserExtension(name: string, template: string, dev: boolean) {
   const extPathToRoot = path.resolve(name);
@@ -68,7 +69,9 @@ function createBrowserExtension(name: string, template: string, dev: boolean) {
 
 function createPackageJson(extPathToRoot: string, extName: string) {
   console.log();
-  console.log(`Creating package.json for ${extName} in ${chalk.green(extPathToRoot)}`);
+  console.log(
+    `Creating package.json for ${extName} in ${chalk.green(extPathToRoot)}`
+  );
   console.log();
 
   const packageJson = {
@@ -76,21 +79,31 @@ function createPackageJson(extPathToRoot: string, extName: string) {
     version: '0.1.0',
   };
 
-  fs.outputJsonSync(path.join(extPathToRoot, 'package.json'), packageJson, { spaces: 2 });
+  fs.outputJsonSync(path.join(extPathToRoot, 'package.json'), packageJson, {
+    spaces: 2,
+  });
 }
 
 function installScripts(extPathToRoot: string, dev: boolean) {
   const command = !dev ? 'install' : 'link';
 
-  console.log(`${command} webextension-scripts in ${chalk.green(extPathToRoot)}`);
+  console.log(
+    `${command} webextension-scripts in ${chalk.green(extPathToRoot)}`
+  );
   console.log();
 
-  spawn.sync('npm', [command, 'webextension-scripts'], { stdio: 'inherit', cwd: extPathToRoot });
+  spawn.sync('npm', [command, 'webextension-scripts'], {
+    stdio: 'inherit',
+    cwd: extPathToRoot,
+  });
 }
 
 function initScript(extPathToRoot: string, template: string) {
   console.log(`Running webextension-scripts init ${template}`);
   console.log();
 
-  spawn.sync('npx', ['webextension-scripts', 'init', template], { stdio: 'inherit', cwd: extPathToRoot });
+  spawn.sync('npx', ['webextension-scripts', 'init', template], {
+    stdio: 'inherit',
+    cwd: extPathToRoot,
+  });
 }
