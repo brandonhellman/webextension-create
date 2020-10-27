459b2bc4744935a0d81e92e33047022b0600c583
Brandon Hellman <me@brandonhellman.com>
2020-10-27 16:01:32 -0700

    Changed scripts

--- packages/webextension-scripts/src/scripts/start.ts
+++ packages/webextension-scripts/src/scripts/start.ts
@@ -1,29 +1,37 @@
-import webpack from 'webpack';
-
 import { config } from '../webpack/config';
+import webpack from 'webpack';
 
 export async function start() {
   const { development } = config();
 
   // @ts-ignore
-  webpack(development).watch({ aggregateTimeout: 300, poll: 1000 }, (err: Error, stats: webpack.Stats) => {
-    if (err) {
-      console.error(err);
-      return;
+  webpack(development).watch(
+    { aggregateTimeout: 300, poll: 1000 },
+    (err: Error, stats: webpack.Stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const info = stats.toJson();
+
+      if (stats.hasErrors()) {
+        console.log('Failed to compile.');
+        console.error(info.errors);
+        return;
+      }
+
+      if (stats.hasWarnings()) {
+        console.warn(info.warnings);
+      }
+
+      console.log(
+        `Compiled in ${
+          stats.endTime && stats.startTime
+            ? stats.endTime - stats.startTime
+            : '?'
+        }ms!`
+      );
     }
-
-    const info = stats.toJson();
-
-    if (stats.hasErrors()) {
-      console.log('Failed to compile.');
-      console.error(info.errors);
-      return;
-    }
-
-    if (stats.hasWarnings()) {
-      console.warn(info.warnings);
-    }
-
-    console.log(`Compiled in ${stats.endTime && stats.startTime ? stats.endTime - stats.startTime : '?'}ms!`);
-  });
+  );
 }
